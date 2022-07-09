12b05b6ad965922f8f0a731fa66bad0c00a0c0ab
goldenstein64 <jamesramsauer@gmail.com>
2022-07-09 03:10:49 -0400

    Removed Minesweeper from DepLoader

--- test/shared/DepLoader/init.lua
+++ test/shared/DepLoader/init.lua
@@ -1,12 +1,11 @@
-local ModuleInstances = {
+local dependencies = {
 	Roact = script.Roact,
 	GoodSignal = script.GoodSignal,
 	Array2D = script.Array2D,
-	Minesweeper = script.Minesweeper,
 }
 
 local function DepLoader(name)
-	return require(ModuleInstances[name])
+	return require(dependencies[name])
 end
 
 return DepLoader

f067b75fe0e04069c3ec4df9e1198973233ac570
goldenstein64 <jamesramsauer@gmail.com>
2022-07-10 04:41:08 -0400

    Made GameLoop.lua a parent

--- src/GameLoop/init.lua
+++ src/GameLoop/init.lua
@@ -1,10 +1,6 @@
 local RunService = game:GetService("RunService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local load = require(ReplicatedStorage.DepLoader)
-local Array2D = load("Array2D")
-
-local R = Random.new()
+local GenerateBoard = require(script.BoardGenerator)
 
 local GameLoop = {}
 
@@ -20,32 +16,8 @@ function GameLoop.start(data, startingCell)
 		end
 	end)
 
-	local size = data.size
-	local openCellSet = Array2D.new()
+	GenerateBoard(data, startingCell)
 
-	openCellSet:set(startingCell.position.X, startingCell.position.Y, true)
-	for _, neighbor in startingCell:getNeighbors() do
-		local newPosition = neighbor.position
-		openCellSet:set(newPosition.X, newPosition.Y, true)
-	end
-
-	for _i = 1, data.mineCount do
-		local x, y
-		repeat
-			x = R:NextInteger(1, size.X)
-			y = R:NextInteger(1, size.Y)
-		until not openCellSet:get(x, y)
-		local cell = data.cells:get(x, y)
-		cell.hasMine = true
-
-		for _, neighbor in cell:getNeighbors() do
-			neighbor.surroundingMines += 1
-		end
-
-		openCellSet:set(x, y, true)
-	end
-
-	data.sideEffects = true
 	startingCell:openSafeCells()
 end
 
@@ -53,8 +25,6 @@ function GameLoop.finish(data, finalCell)
 	assert(data.timerConn, "game is not started yet!")
 	data.timerConn:Disconnect()
 	data.timerConn = nil
-
-	data.sideEffects = false
 
 	if finalCell then
 		finalCell:setState("mineHit")
@@ -77,7 +47,6 @@ function GameLoop.reset(data)
 		data.timerConn = nil
 	end
 
-	data.sideEffects = false
 	data.setTime(0)
 	data.setMinesLeft(data.mineCount)
 	data.cellsLeft = data.size.X * data.size.Y - data.mineCount
